d861a620768b5bbbee605b8567fc6c9ed401a0b2
faizmohammad <faizmohammad@mcirosoft.com>
2024-03-17 02:38:45 +0530

    added comment for explaination

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+//heading is an object of type h1 with id as props and "Hello world from React!" as children.
 const heading = React.createElement('h1', {
     id:"heading"
 }, "Hello world from React!")
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(heading)
