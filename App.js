78c95c3a20146751b398169bf70a5021f54db114
faizmohammad <faizmohammad@mcirosoft.com>
2024-03-17 23:44:14 +0530

    added first nested element

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
+/*
+<div id = "parent">
+    <div id="child">
+        <h1></h1>
+    </div>
+</div>
+*/
+
+const parent = React.createElement('div',
+    {id:"parent"},
+    React.createElement('div',
+        {id:"child"},
+        React.createElement(
+            'h1',
+            {},
+            "First nested react element"
+        )
+    )
+)
+
 //heading is an object of type h1 with id as props and "Hello world from React!" as children.
 const heading = React.createElement('h1', {
     id:"heading"
 }, "Hello world from React!")
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+root.render(parent)
