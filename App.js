7355f50534ae87f7b4a03eb2ac688971b865183f
faizmohammad <faizmohammad@mcirosoft.com>
2024-03-24 20:01:27 +0530

    added more hierarchy to react tree structure

--- App.js
+++ App.js
@@ -4,20 +4,30 @@
         <h1>First nested react element</h1>
         <h1>Second nested react element</h1>
     </div>
+    <div id="child2">
+        <h1> I am h1 tag</h1>
+        <h2> I am h2 tag</h2>
+    </div>
 </div>
 */
 
 const parent = React.createElement('div',
     {id:"parent"},
-    React.createElement('div',
-        {id:"child"},
+    [
+        React.createElement('div', {id:"child"},
 
         //passing sibling elements in array format
         [
             React.createElement('h1',{},"First nested react element"),
-            React.createElement('h1',{id:"second-child"},"I am second nested element")
+            React.createElement('h2',{},"I am second nested element")
         ]
-    )
+    ),
+        React.createElement('div', {id:'child2'},
+        [
+            React.createElement('h1',{}, 'I am h1 tag'),
+            React.createElement('h2',{}, 'I am h2 tag')
+        ])
+    ]
 )
 
 //heading is an object of type h1 with id as props and "Hello world from React!" as children.
