b3c505dab528fab680066f7915cba0ba710d1d2d
faizmohammad <faizmohammad@mcirosoft.com>
2024-03-24 20:03:29 +0530

    fixed identation

--- App.js
+++ App.js
@@ -32,8 +32,10 @@ const parent = React.createElement('div',
 
 //heading is an object of type h1 with id as props and "Hello world from React!" as children.
 const heading = React.createElement('h1', {
-    id:"heading"
-}, "Hello world from React!")
+        id:"heading"
+    }, 
+    "Hello world from React!"
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(parent)
